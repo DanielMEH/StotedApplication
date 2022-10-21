8099b58c5d571f99408e9f75a4cd12464db520b2
Angie Mejia <mejiagonzaleza881@gmail.com>
2022-10-21 11:51:00 -0500

    application src

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Alert, Text, View,TextInput, TouchableNativeFeedback } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableNativeFeedback
+      onPress={()=> Alert.alert("Hola mundo")}
+      >
+      <Text> MY appp en react-app con expo</Text>
+      </TouchableNativeFeedback > 
+
     </View>
   );
 }
